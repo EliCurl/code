8f119b7119425053daad021e194b82657a54d31a
EliCurl <curle4271@my.uwstout.edu>
2024-09-11 11:34:40 -0500

    NewStuff1

--- Software Engineering Principles/HelloWorld.java
+++ Software Engineering Principles/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld{
     public static void main(String[] args) {
         String words = "Hello World";
         System.out.println(words);
+        String salami = "This is so crazy";
+        words = words + " " + salami;
     }
 }
